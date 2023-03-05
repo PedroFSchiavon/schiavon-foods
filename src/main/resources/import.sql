4143876c5781ca243cdf55f0179924916238c25f
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-03-05 14:48:46 -0300

    Criando inserts de teste padrões.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cozinha (nome) values ('Amazonense');
+insert into cozinha (nome) values ('Capixaba');
+insert into restaurante (nome, taxa_frete) values ('Cantinho do Burguer', 4.76);
+insert into restaurante (nome, taxa_frete) values ('Vini burguer', 3.80);
