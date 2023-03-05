0272d543dfb831a31992c71c40c2dd8df8db8a7c
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-03-05 16:55:18 -0300

    Mapeando entidade de permissão.

---
+++ src/main/java/br/com/schiavon/food/domain/models/Permissao.java
@@ -0,0 +1,22 @@
+package br.com.schiavon.food.domain.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permissao implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+}
