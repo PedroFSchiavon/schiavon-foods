9a0552024391e3c06c13e1326f8f2eeeb8fcb9aa
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-03-05 17:02:45 -0300

    criando repositories das classes Estado, Cidade e Permissao.

--- src/main/java/br/com/schiavon/food/domain/repositories/CidadeRepository.java
+++ src/main/java/br/com/schiavon/food/domain/repositories/CidadeRepository.java
@@ -1,9 +1,9 @@
 package br.com.schiavon.food.domain.repositories;
 
-import br.com.schiavon.food.domain.models.Restaurante;
+import br.com.schiavon.food.domain.models.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 }
