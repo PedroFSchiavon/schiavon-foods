6ec3ff1f8e96347351e7137db63780b0069e3517
Pedro F Schiavon <pedrofschiavon@gmail.com>
2023-04-27 22:27:20 -0300

    Criando controller de cozinhas

---
+++ src/main/java/br/com/schiavon/food/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package br.com.schiavon.food.api.controller;
+
+import br.com.schiavon.food.domain.models.Cozinha;
+import br.com.schiavon.food.domain.repositories.CozinhaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+    private final CozinhaRepository cozinhaRepository;
+
+    public CozinhaController(CozinhaRepository cozinhaRepository){
+        this.cozinhaRepository = cozinhaRepository;
+    }
+
+    @GetMapping
+    public List<Cozinha> listar(){
+        return cozinhaRepository.findAll();
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
 insert into cozinha (nome) values ('Amazonense');
 insert into cozinha (nome) values ('Mineira');
+insert into cozinha (nome) values ('Italiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Cantinho do Burguer', 4.76, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Vini burguer', 3.80, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Paçai', 7.80, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Forno da saudade', 1.15, 3);
