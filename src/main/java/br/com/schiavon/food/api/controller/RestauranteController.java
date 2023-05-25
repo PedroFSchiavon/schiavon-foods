f55aa7d449062c5db24dcda8b521e3bdfbfd2be7
Pedro F Schiavon <pedrofschiavon@gmail.com>
2023-05-24 22:36:28 -0300

    Criando named Querrys por xml.

--- src/main/java/br/com/schiavon/food/api/controller/RestauranteController.java
+++ src/main/java/br/com/schiavon/food/api/controller/RestauranteController.java
@@ -29,6 +29,11 @@ public class RestauranteController {
         return restauranteRepository.findAll();
     }
 
+    @GetMapping("/nome")
+    public List<Restaurante> buscarPorNome(@RequestParam String nome){
+        return restauranteRepository.buscarPorNome(nome);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Restaurante> buscar(@PathVariable Long id){
         Optional<Restaurante> restauranteOptional = restauranteRepository.findById(id);
--- src/main/java/br/com/schiavon/food/domain/repositories/RestauranteRepository.java
+++ src/main/java/br/com/schiavon/food/domain/repositories/RestauranteRepository.java
@@ -2,8 +2,12 @@ package br.com.schiavon.food.domain.repositories;
 
 import br.com.schiavon.food.domain.models.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    List<Restaurante> buscarPorNome(String nome);
 }
