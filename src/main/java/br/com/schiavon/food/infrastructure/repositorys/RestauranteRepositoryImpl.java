08202445ababc7bb59a9715f9692747287954bd6
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-05-28 15:24:09 -0300

    Criando querrys de restaurante com CriteriaAPI.

--- src/main/java/br/com/schiavon/food/infrastructure/repositorys/RestauranteRepositoryImpl.java
+++ src/main/java/br/com/schiavon/food/infrastructure/repositorys/RestauranteRepositoryImpl.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +22,23 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
 
     @Override
     public List<Restaurante> buscaPorNomeETaxa(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal){
-        StringBuilder jpql = new StringBuilder("from Restaurante as r where 1=1 ");
+        CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+        CriteriaQuery<Restaurante> criteria = criteriaBuilder.createQuery(Restaurante.class);
+
+        Root<Restaurante> root = criteria.from(Restaurante.class);
+
+        Predicate likeNome = criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+        Predicate maiorTaxaInicial = criteriaBuilder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaInicial);
+        Predicate menorTaxaFinal = criteriaBuilder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFinal);
+
+        criteria.where(likeNome, maiorTaxaInicial, menorTaxaFinal);
+
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
+
+        return query.getResultList();
+
+        //QUERY DINAMICA COM JPQL
+        /*StringBuilder jpql = new StringBuilder("from Restaurante as r where 1=1 ");
         HashMap<String, Object> parametros = new HashMap<>();
 
         if (nome != null && !nome.isBlank()){
@@ -37,8 +57,8 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQuery {
 
         TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
 
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+        parametros.forEach(query::setParameter);
 
-        return query.getResultList();
+        return query.getResultList();*/
     }
 }
