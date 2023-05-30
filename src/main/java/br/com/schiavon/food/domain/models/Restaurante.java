ef7006506770746198c4fdd55567460fae6a4ba0
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-05-30 11:53:48 -0300

    Alterando o relacionamento entre Restaurante e FormaPagamento para ManytoMany. Gerando exemplos na tabela para esse relacionamento.

--- src/main/java/br/com/schiavon/food/domain/models/Restaurante.java
+++ src/main/java/br/com/schiavon/food/domain/models/Restaurante.java
@@ -22,7 +22,10 @@ public class Restaurante implements Serializable {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
-    @OneToMany
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+    joinColumns = @JoinColumn(name = "restaurante_id"),
+    inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formaPagamento;
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,27 @@
+-- cozinha
 insert into cozinha (nome) values ('Amazonense');
 insert into cozinha (nome) values ('Mineira');
 insert into cozinha (nome) values ('Italiana');
+-- restaurante
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Cantinho do Burguer', 4.76, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Vini burguer', 3.80, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Paçai', 7.80, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Forno da saudade', 1.15, 3);
+-- estado
 insert into estado (nome) values ('Minas Gerais');
 insert into estado (nome) values ('São Paulo');
 insert into estado (nome) values ('Rio Grande do Sul');
 insert into estado (nome) values ('Santa Catarina');
+-- cidade
 insert into cidade (nome, estado_id) values ('Campinas', 2);
 insert into cidade (nome, estado_id) values ('Porto Alegre', 3);
 insert into cidade (nome, estado_id) values ('Florianopolis', 4);
 insert into cidade (nome, estado_id) values ('Belo Horizonte', 1);
+-- forma de pagamento
+insert into forma_pagamento (descricao) values ('Cartão de crédito');
+insert into forma_pagamento (descricao) values ('Cartão de débito');
+insert into forma_pagamento (descricao) values ('Pix');
+insert into forma_pagamento (descricao) values ('Dinheiro');
+insert into forma_pagamento (descricao) values ('Boleto');
+-- Restaurante e forma de pagamento
+insert into restaurante_forma_pagamento values (1,1), (1,2), (1,3), (1,4), (2,1), (2,2), (2,3), (3,2), (3,4), (4,5);
