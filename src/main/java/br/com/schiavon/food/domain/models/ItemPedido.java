79000ae3e3231fd850277aea9024141ef48a5d05
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-06-11 14:35:19 -0300

    Criando entidade ItemPedido.

---
+++ src/main/java/br/com/schiavon/food/domain/models/ItemPedido.java
@@ -0,0 +1,7 @@
+package br.com.schiavon.food.domain.models;
+
+import java.io.Serializable;
+
+public class ItemPedido implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
