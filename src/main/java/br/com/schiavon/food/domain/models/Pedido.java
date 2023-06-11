9e7232ef4c56c25fee99bf6ff3faa2eebe72870a
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-06-11 13:59:37 -0300

    Criando entidade Pedido sem seu relacionamento.

---
+++ src/main/java/br/com/schiavon/food/domain/models/Pedido.java
@@ -0,0 +1,43 @@
+package br.com.schiavon.food.domain.models;
+
+import br.com.schiavon.food.domain.models.enuns.StatusPedido;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pedido implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+
+    @Column(nullable = false)
+    private BigDecimal taxaFrete;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCriacao;
+
+    private LocalDateTime dataConfirmacao;
+
+    private LocalDateTime dataCancelamento;
+
+    private LocalDateTime dataEntrega;
+
+    @Enumerated(value = EnumType.STRING)
+    private StatusPedido statusPedido;
+
+}
