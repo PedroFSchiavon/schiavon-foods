d93f7807a6417641edb4d44fc62c0c4f80652070
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-06-13 18:20:38 -0300

    Adicionando a anotação ResponseStatus nas exceções.

--- src/main/java/br/com/schiavon/food/domain/exceptions/RelacionamentoEntidadeNaoEncontradoException.java
+++ src/main/java/br/com/schiavon/food/domain/exceptions/RelacionamentoEntidadeNaoEncontradoException.java
@@ -1,5 +1,9 @@
 package br.com.schiavon.food.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RelacionamentoEntidadeNaoEncontradoException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
