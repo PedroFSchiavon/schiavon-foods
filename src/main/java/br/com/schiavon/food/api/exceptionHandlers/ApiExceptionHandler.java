53f62005425ab356439ddd60a4fb1aa7cf240e8c
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-07-07 18:55:25 -0300

    Padronizando lançamento de Exception com handleExceptionInternal.

--- src/main/java/br/com/schiavon/food/api/exceptionHandlers/ApiExceptionHandler.java
+++ src/main/java/br/com/schiavon/food/api/exceptionHandlers/ApiExceptionHandler.java
@@ -3,31 +3,44 @@ package br.com.schiavon.food.api.exceptionHandlers;
 import br.com.schiavon.food.domain.exceptions.EntidadeEmUsoException;
 import br.com.schiavon.food.domain.exceptions.EntidadeNaoEncontradaException;
 import br.com.schiavon.food.domain.exceptions.RelacionamentoEntidadeNaoEncontradoException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e){
-        Problema problema = new Problema(e.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    public ResponseEntity<?>
+    trataEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest webRequest){
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
     }
 
     @ExceptionHandler(RelacionamentoEntidadeNaoEncontradoException.class)
     public ResponseEntity<?>
-    trataRelacionamentoEntidadeNaoEncontradoException(RelacionamentoEntidadeNaoEncontradoException e){
-        Problema problema = new Problema(e.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    trataRelacionamentoEntidadeNaoEncontradoException(RelacionamentoEntidadeNaoEncontradoException e, WebRequest webRequest){
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> trataEntidadeEmUsoException(EntidadeEmUsoException e){
-        Problema problema = new Problema(e.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    public ResponseEntity<?> trataEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest webRequest){
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        if(body == null){
+            body = new Problema(status.getReasonPhrase(), LocalDateTime.now());
+        } else if (body instanceof String) {
+            body = new Problema((String) body, LocalDateTime.now());
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
