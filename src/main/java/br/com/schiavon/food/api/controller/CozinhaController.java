706c13566a52717f9c23384379fdec6e6d35686c
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-08-10 15:47:50 -0300

    Criando testes para cadastro cozinha e application.properties para test.

--- src/main/java/br/com/schiavon/food/api/controller/CozinhaController.java
+++ src/main/java/br/com/schiavon/food/api/controller/CozinhaController.java
@@ -4,11 +4,19 @@ import br.com.schiavon.food.api.model.CozinhasXMLWrapper;
 import br.com.schiavon.food.domain.models.Cozinha;
 import br.com.schiavon.food.domain.repositories.CozinhaRepository;
 import br.com.schiavon.food.domain.services.CozinhaService;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
--- src/test/java/br/com/schiavon/food/CadastroCozinhaIT.java
+++ src/test/java/br/com/schiavon/food/CadastroCozinhaIT.java
@@ -1,17 +1,56 @@
 package br.com.schiavon.food;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import static io.restassured.RestAssured.given;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
-    @Test
-    public void deveRetornar200_QuandoConsultarCozinha(){
+    @Autowired
+    private Flyway flyway;
 
+    @BeforeEach
+    public void configurations() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
+    }
+
+    @Test
+    public void deveRetornar200_QuandoConsultarCozinha() {
+        given().accept(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinha() {
+        given().accept(ContentType.JSON)
+                .when().get()
+                .then().body("", Matchers.hasSize(3)).body("nome", Matchers.hasItems("Mineira", "Amazonense"));
+    }
+
+    @Test
+    public void deveRetornar201_QuandoCadastrarUmaCozinha(){
+        given().body("{\"nome\": \"Inglesa\"}").contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when().post()
+                .then().statusCode(HttpStatus.CREATED.value());
     }
 }
