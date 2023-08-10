ce34b871630a514f97612fa8bb38dc6f33687b3c
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-08-10 11:10:41 -0300

    Refatorando testes de integração e simplificando a lógica.

--- src/test/java/br/com/schiavon/food/CadastroCozinhaIntegrationIT.java
+++ src/test/java/br/com/schiavon/food/CadastroCozinhaIntegrationIT.java
@@ -1,5 +1,7 @@
 package br.com.schiavon.food;
 
+import br.com.schiavon.food.domain.exceptions.CozinhaNaoEncontradaException;
+import br.com.schiavon.food.domain.exceptions.EntidadeEmUsoException;
 import br.com.schiavon.food.domain.models.Cozinha;
 import br.com.schiavon.food.domain.services.CozinhaService;
 import org.assertj.core.api.Assertions;
@@ -7,7 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -33,29 +38,28 @@ public class CadastroCozinhaIntegrationIT {
         Exception exception = assertThrows(Exception.class,
                 () -> cozinhaService.cadastro(cozinha));
 
-        String menssagemObtida = exception.getMessage();
-        String menssagemCorreta = "Validation failed for classes";
-
-        assertTrue(menssagemObtida.contains(menssagemCorreta));
+        assertTrue(exception instanceof ConstraintViolationException);
     }
 
     @Test
     public void deveFalhar_QuandoCozinhaEstiverEmUso(){
         Exception exception = assertThrows(Exception.class,
                 () ->  cozinhaService.deletar(1L));
-        String menssagemObtida = exception.getMessage();
-        String menssagemCorreta = "Não foi possível deletar";
+//        String menssagemObtida = exception.getMessage();
+//        String menssagemCorreta = "Não foi possível deletar";
 
-        assertTrue(menssagemObtida.contains(menssagemCorreta));
+        assertTrue(exception instanceof EntidadeEmUsoException);
+        //assertTrue(menssagemObtida.contains(menssagemCorreta));
     }
 
     @Test
     public void deveFalhar_QuandoExcluirCozinhaInexistente(){
         Exception exception = assertThrows(Exception.class,
                 () ->  cozinhaService.deletar(22L));
-        String menssagemObtida = exception.getMessage();
-        String menssagemCorreta = "não foi encontrada";
+//        String menssagemObtida = exception.getMessage();
+//        String menssagemCorreta = "não foi encontrada";
 
-        assertTrue(menssagemObtida.contains(menssagemCorreta));
+        assertTrue(exception instanceof CozinhaNaoEncontradaException);
+        //assertTrue(menssagemObtida.contains(menssagemCorreta));
     }
 }
