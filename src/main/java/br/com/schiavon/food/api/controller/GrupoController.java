ff32ead0ed648f30d7a6a1febc355c602595bddb
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-08-30 16:26:01 -0300

    Finalizando endpoint de grupos.

--- src/main/java/br/com/schiavon/food/api/controller/GrupoController.java
+++ src/main/java/br/com/schiavon/food/api/controller/GrupoController.java
@@ -5,10 +5,17 @@ import br.com.schiavon.food.api.model.dto.output.GrupoDTO;
 import br.com.schiavon.food.domain.models.Grupo;
 import br.com.schiavon.food.domain.repositories.GrupoRepository;
 import br.com.schiavon.food.domain.services.GrupoService;
+import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -37,6 +44,25 @@ public class GrupoController {
     @GetMapping("/{idGrupo}")
     public GrupoDTO buscar(@PathVariable Long idGrupo){
         return toDTO(grupoService.buscarGrupoId(idGrupo));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GrupoDTO cadastrar(@RequestBody @Valid GrupoInputDTO grupoInputDTO){
+        Grupo grupo = toDomainModel(grupoInputDTO);
+        return toDTO(grupoService.cadastrar(grupo));
+    }
+
+    @PutMapping("/{idGrupo}")
+    public GrupoDTO atualizar(@PathVariable Long idGrupo, @RequestBody @Valid GrupoInputDTO grupoInputDTO){
+        Grupo grupo = toDomainModel(grupoInputDTO);
+        return toDTO(grupoService.atualizar(idGrupo, grupo));
+    }
+
+    @DeleteMapping("/{idGrupo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long idGrupo){
+        grupoService.deletar(idGrupo);
     }
 
     private GrupoDTO toDTO(Grupo grupo){
--- src/main/java/br/com/schiavon/food/domain/services/GrupoService.java
+++ src/main/java/br/com/schiavon/food/domain/services/GrupoService.java
@@ -1,16 +1,47 @@
 package br.com.schiavon.food.domain.services;
 
+import br.com.schiavon.food.domain.exceptions.EntidadeEmUsoException;
 import br.com.schiavon.food.domain.exceptions.GrupoNaoEncontradaException;
 import br.com.schiavon.food.domain.models.Grupo;
 import br.com.schiavon.food.domain.repositories.GrupoRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GrupoService {
     private final GrupoRepository grupoRepository;
 
+    public static final String GRUPO_ID_EM_USO = "Não foi possível deletar a grupo com o id %d," +
+            " pois esta em uso no momento.";
+
     public GrupoService(GrupoRepository grupoRepository){
         this.grupoRepository = grupoRepository;
+    }
+
+    @Transactional
+    public Grupo cadastrar(Grupo grupo){
+        return grupoRepository.save(grupo);
+    }
+
+    @Transactional
+    public Grupo atualizar(Long id, Grupo grupo){
+        if(grupoRepository.existsById(id)){
+            grupo.setId(id);
+            return grupoRepository.save(grupo);
+        }
+        throw new GrupoNaoEncontradaException(id);
+    }
+
+    @Transactional
+    public void deletar(Long id){
+        Grupo grupo = buscarGrupoId(id);
+        try{
+            grupoRepository.delete(grupo);
+            grupoRepository.flush();
+        }catch (DataIntegrityViolationException e){
+            throw new EntidadeEmUsoException(String.format(GRUPO_ID_EM_USO, id));
+        }
     }
 
     public Grupo buscarGrupoId(Long id){
