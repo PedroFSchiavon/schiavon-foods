94e49124166551de20f1809bebf5371b70c93985
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-09-23 15:17:23 -0300

    Finalizando a implementação do endpoint Usuario+Grupo.

--- src/main/java/br/com/schiavon/food/api/controller/UsuarioGrupoController.java
+++ src/main/java/br/com/schiavon/food/api/controller/UsuarioGrupoController.java
@@ -1,9 +1,22 @@
 package br.com.schiavon.food.api.controller;
 
+import br.com.schiavon.food.api.model.dto.output.GrupoDTO;
+import br.com.schiavon.food.domain.models.Grupo;
+import br.com.schiavon.food.domain.models.Usuario;
 import br.com.schiavon.food.domain.services.UsuarioGrupoService;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios/{idUsuario}/grupos")
@@ -15,5 +28,33 @@ public class UsuarioGrupoController {
     public UsuarioGrupoController(UsuarioGrupoService usuarioGrupoService, ModelMapper modelMapper) {
         this.usuarioGrupoService = usuarioGrupoService;
         this.modelMapper = modelMapper;
+    }
+
+    @GetMapping
+    public List<GrupoDTO> listar(@PathVariable Long idUsuario){
+        Usuario usuario = usuarioGrupoService.buscarUsuarioId(idUsuario);
+
+        return toCollectionDTO(usuario.getGrupos());
+    }
+
+    @PutMapping("/{idGrupo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void associarGrupo(@PathVariable Long idUsuario, @PathVariable Long idGrupo){
+        usuarioGrupoService.associarGrupo(idUsuario, idGrupo);
+    }
+
+    @DeleteMapping("/{idGrupo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void desassociarGrupo(@PathVariable Long idUsuario, @PathVariable Long idGrupo){
+        usuarioGrupoService.desassociarGrupo(idUsuario, idGrupo);
+    }
+
+
+    private GrupoDTO toDTO(Grupo grupo){
+        return modelMapper.map(grupo, GrupoDTO.class);
+    }
+
+    private List<GrupoDTO> toCollectionDTO(Set<Grupo> grupos){
+        return grupos.stream().map(this::toDTO).collect(Collectors.toList());
     }
 }
--- src/main/java/br/com/schiavon/food/domain/services/UsuarioGrupoService.java
+++ src/main/java/br/com/schiavon/food/domain/services/UsuarioGrupoService.java
@@ -1,6 +1,9 @@
 package br.com.schiavon.food.domain.services;
 
+import br.com.schiavon.food.domain.models.Grupo;
+import br.com.schiavon.food.domain.models.Usuario;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioGrupoService {
@@ -11,5 +14,27 @@ public class UsuarioGrupoService {
     public UsuarioGrupoService(UsuarioService usuarioService, GrupoService grupoService) {
         this.usuarioService = usuarioService;
         this.grupoService = grupoService;
+    }
+
+    @Transactional
+    public void associarGrupo(Long idUsuario, Long idGrupo){
+        Usuario usuario = buscarUsuarioId(idUsuario);
+        Grupo grupo = grupoService.buscarGrupoId(idGrupo);
+
+        usuario.getGrupos().add(grupo);
+    }
+
+    @Transactional
+    public void desassociarGrupo(Long idUsuario, Long idGrupo){
+        Usuario usuario = buscarUsuarioId(idUsuario);
+        Grupo grupo = grupoService.buscarGrupoId(idGrupo);
+
+        usuario.getGrupos().remove(grupo);
+    }
+
+
+
+    public Usuario buscarUsuarioId(Long id){
+        return usuarioService.buscarUsuarioId(id);
     }
 }
