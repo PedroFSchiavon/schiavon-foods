6ce7e1a857b6235c599ce39d6cde1a4fb70cda0a
PedroFSchiavon <pedrofschiavon@gmail.com>
2023-10-07 18:44:46 -0300

    Verificando bug em cadastro de pedido

--- src/main/java/br/com/schiavon/food/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/schiavon/food/core/modelmapper/ModelMapperConfig.java
@@ -1,7 +1,9 @@
 package br.com.schiavon.food.core.modelmapper;
 
+import br.com.schiavon.food.api.model.dto.input.ItemPedidoInputDTO;
 import br.com.schiavon.food.api.model.dto.output.EnderecoDTO;
 import br.com.schiavon.food.domain.models.Endereco;
+import br.com.schiavon.food.domain.models.ItemPedido;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +12,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
     @Bean
-    public ModelMapper modelMapperBean(){
+    public ModelMapper modelMapperBean() {
         ModelMapper modelMapper = new ModelMapper();
 
         TypeMap<Endereco, EnderecoDTO> typeMapEndereco = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
 
         typeMapEndereco.addMapping((src) -> src.getCidade().getNome(), EnderecoDTO::setCidade);
 
-       typeMapEndereco.addMapping((src) -> src.getCidade().getEstado().getNome(), EnderecoDTO::setEstado);
+        typeMapEndereco.addMapping((src) -> src.getCidade().getEstado().getNome(), EnderecoDTO::setEstado);
+
+        //modelMapper.createTypeMap(ItemPedidoInputDTO.class, ItemPedido.class).addMappings(mapper -> mapper.skip(ItemPedido::setId));
 
         return modelMapper;
     }
